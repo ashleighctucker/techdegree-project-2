19f6cdb7c4b25a60bb5193b1fdf0c15477a9c188
ashleighctucker <ashleighctucker@gmail.com>
2021-01-27 10:13:09 -0800

    fixed valueerror issues
    replaced input with raw_input

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def pick_team(teams_list):
         print("2)  Bandits")
         print("3)  Warriors")
         try:
-            choice = int(input("\nEnter a team option:   "))
+            choice = int(raw_input("\nEnter a team option:   "))
             if choice < 1:
                 raise ValueError()
             if choice > 3:
@@ -116,13 +116,12 @@ def pick_team(teams_list):
                     break
 
 
-
 def start_tool():
 
     while True:
         print("Here are your choices:\n \n1)  Display Team Stats\n2)  Quit\n")
         try:
-            option = int(input("Enter an option:   "))
+            option = int(raw_input("Enter an option:   "))
             if option < 1:
                 raise ValueError()
             if option > 2:
